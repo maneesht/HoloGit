d6b9910582b8ec483294f21900c65e12405403eb
Andrew <Bostockdrew97@gmail.com>
2017-09-24 18:50:46 -0400

    Added functionality to get repositories of specific users or popular repos

---
+++ server/src/gitpoller.ts
@@ -0,0 +1,119 @@
+//import * as request from 'request';
+import * as request from 'request-promise-native';
+import * as q from 'q';
+
+export class GitPoller {
+    
+    static option = {
+        headers: {'User-agent': 'hologit/0.1'},
+        json: true
+    };
+
+    static getRepo(username: string, repo: string) {
+        let options = Object.assign(GitPoller.option, {
+            url: `https://api.github.com/repos/${username}/${repo}/branches`
+        });
+        let branches: {branchId: string, commits: object, parentBranch: string}[] = [];
+        return request.get(options).then((body:any) => {
+            let promises:Promise<any>[] = [];
+            body.forEach((branch: JSON) => {
+                let promise = GitPoller.getCommits(username, repo, branch['name']).then((data: object) => {
+                    branches.push({
+                        branchId: branch['name'],
+                        commits: data,
+                        parentBranch: ''
+                    });
+                });
+                promises.push(promise);
+            });
+            return q.all(promises).then(data => branches);
+        });
+    }
+
+    static getBranch(username: string, repo: string, branch: string) {
+        let branchInfo: {branchID: {commits: object}};
+        return GitPoller.getCommits(username, repo, branch).then(data => {
+            branchInfo.branchID.commits = data;
+            return branchInfo;
+        });
+    }
+
+    static getCommits(username: string, repo: string, branch: string) {
+        let options = Object.assign(GitPoller.option, {
+            url: `https://api.github.com/repos/${username}/${repo}/commits?sha=${branch}`
+        });
+        let commits: {sha: string, author: string, message: string, parentSha: string}[] = [];
+        return request.get(options).then(body => {
+            body.forEach((commit: JSON) => {
+                let pSha: string = '';
+                if (commit['parents'].length != 0) {
+                    pSha = commit['parents'][0]['sha'];
+                }
+                commits.push({
+                    sha: commit['sha'],
+                    author: commit['commit']['author']['name'],
+                    message: commit['commit']['message'],
+                    parentSha: pSha
+                });
+            });
+            return commits;
+        });
+    }
+
+    static getCommit(username: string, repo: string, sha: string) {
+        let options = Object.assign(GitPoller.option, {
+            url: `https://api.github.com/repo/${username}/${repo}/commits?sha=${sha}`
+        });
+        let commit: {commitId: {branchID: string, author: string, committer: string, parentSha: string}}
+        return request.get(options).then(body => {
+            let pSha: string = '';
+            if (body['parents'].length != 0) {
+                pSha = body['parents'][0]['sha'];
+            }
+            commit.commitId = {
+                branchID: '',
+                author: body['commit']['author']['name'],
+                committer: body['commit']['committer']['name'],
+                parentSha: pSha
+            };
+            return commit
+        });
+    }
+
+    static getPopularRepos() {
+        let date = new Date((new Date()).getTime() - 604800000);
+        let options = Object.assign(GitPoller.option, {
+            url: `https://api.github.com/search/repositories?sort=stars&order=desc&q=created:>${date.toISOString().split('T')[0]}`
+        });
+        let data: {name: string, description: string, language: string, owner: string}[] = [];
+        return request.get(options).then(body => {
+            body['items'].forEach((repo: JSON) => {
+                data.push({
+                    name: repo['name'],
+                    description: repo['description'],
+                    language: repo['language'],
+                    owner: repo['owner']['login']
+                });
+            });
+            return data;
+        });
+    }
+
+    static getReposByUser(username: string) {
+        let options = Object.assign(GitPoller.option, {
+            url: `https://api.github.com/users/${username}/repos`
+        });
+        let data: {name: string, description: string, language: string, owner: string}[] = [];
+        return request.get(options).then(body => {
+            body.forEach((repo: JSON) => {
+                data.push({
+                    name: repo['name'],
+                    description: repo['description'],
+                    language: repo['language'],
+                    owner: repo['owner']['login']
+                });
+            });
+            return data;
+        });
+    }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,35 @@
 import * as express from 'express';
+import * as request from 'request';
+import { GitPoller } from './gitpoller';
 
 export const routes: express.Router = express.Router();
 routes.get('/', (req: express.Request , res: express.Response, next: express.NextFunction) => {
     res.send('Hello World!');
 });
 // insert other APIs here
+
+//Gets popular public github repositories in the last week
+routes.get('/api/remote/repositories/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    GitPoller.getPopularRepos().then(data => res.send(data));
+});
+
+//Get all the public repositories of the specified user
+routes.get('/api/remote/users/:username/repositories/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    GitPoller.getReposByUser(req.params.username).then(data => res.send(data));
+});
+
+routes.get('/api/remote/users/:username/repositories/:repo', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    GitPoller.getRepo(req.params.username, req.params.repo).then(data => res.send(data));
+});
+
+routes.get('/api/remote/users/:username/repositories/:repo/branches/:branch', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    GitPoller.getBranch(req.params.username, req.params.repo, req.params.branch).then(data => res.send(data));
+});
+
+routes.get('/api/remote/users/:username/repositories/:repo/branches/:branch/commits', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    GitPoller.getCommits(req.params.username, req.params.repo, req.params.branch).then(data => res.send(data));
+});
+
+routes.get('/api/remote/users/:username/repositories/:repo/commits/:commit', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    GitPoller.getCommit(req.params.username, req.params.repo, req.params.commit).then(data => res.send(data));
+});
