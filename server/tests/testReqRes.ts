e0293ee403e8174a5525336cea5b9e98840a408e
Andrew <Bostockdrew97@gmail.com>
2017-10-04 21:32:07 -0400

    created integration testing server

--- server/tests/testReqRes.ts
+++ server/tests/testReqRes.ts
@@ -205,8 +205,9 @@ export const test3res = {
   }
 };
 
-export const test4req = `query	{
-  branches (username: "maneesht" repo: "todo-app" branchName: "master"){
+export const test4req = `{
+  branches (username: "maneesht" repo: "quiz-app"){
+    id
     commits {
       sha
       author
@@ -220,39 +221,34 @@ export const test4res = {
   "data": {
     "branches": [
       {
+        "id": "master",
         "commits": [
           {
-            "sha": "515303528685e1ff575824ab6c187fef19e07334",
+            "sha": "ee847e34adc844304a05172873a98062cacf8b3e",
             "author": "Maneesh Tewani",
-            "message": "Separated todo app",
-            "parentSha": "3d1c24c101b7e3cb9755acc6e1cf03a137c3adc5"
+            "message": "Fixed error with Subscription",
+            "parentSha": "d80cdfc8d0c04a103ca40574f968be659df38d13"
           },
           {
-            "sha": "3d1c24c101b7e3cb9755acc6e1cf03a137c3adc5",
+            "sha": "d80cdfc8d0c04a103ca40574f968be659df38d13",
             "author": "Maneesh Tewani",
-            "message": "Modularized State Management",
-            "parentSha": "6ba70d5827147ac91ee6e16196d625971d5e5790"
+            "message": "Major state and design overhaul",
+            "parentSha": "19789cddb26844c1556d8aa4e6c4476d8e8861c7"
           },
           {
-            "sha": "6ba70d5827147ac91ee6e16196d625971d5e5790",
+            "sha": "19789cddb26844c1556d8aa4e6c4476d8e8861c7",
             "author": "Maneesh Tewani",
-            "message": "Fixed layout and socket update",
-            "parentSha": "f61f2bc354c25b68869e51ac4fa0cf756a991072"
+            "message": "Updated REAMDE",
+            "parentSha": "01f1663604f7d159e75f470bec16960f17aa2f92"
           },
           {
-            "sha": "f61f2bc354c25b68869e51ac4fa0cf756a991072",
+            "sha": "01f1663604f7d159e75f470bec16960f17aa2f92",
             "author": "Maneesh Tewani",
-            "message": "Persistent Firebase storage now works.",
-            "parentSha": "4c88e185398f6f165880188324605cb7fc6a5a39"
+            "message": "Finished with version 1 of QuizApp",
+            "parentSha": "6256442fa231548a3bd1c6a691796aab38bea936"
           },
           {
-            "sha": "4c88e185398f6f165880188324605cb7fc6a5a39",
-            "author": "Maneesh Tewani",
-            "message": "Initial Commit",
-            "parentSha": "d15052a8b84fbbebf07e1aea925f09915a8c9d45"
-          },
-          {
-            "sha": "d15052a8b84fbbebf07e1aea925f09915a8c9d45",
+            "sha": "6256442fa231548a3bd1c6a691796aab38bea936",
             "author": "angular-cli",
             "message": "chore: initial commit from angular-cli",
             "parentSha": ""
