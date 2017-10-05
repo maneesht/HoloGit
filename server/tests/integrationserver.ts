f25da2e42d3e9bc540451c75b28ec774e58e6e6a
Andrew <Bostockdrew97@gmail.com>
2017-10-04 21:33:38 -0400

    created an integration testing server

---
+++ server/tests/integrationserver.ts
@@ -0,0 +1,41 @@
+import * as express from 'express';
+import {GraphQLInt, GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString, graphql } from 'graphql';
+import {CommitQL, BranchQL, queryType} from '../src/app'
+import * as request from 'request-promise-native'
+import * as graphqlHTTP from 'express-graphql';
+
+const schema = new GraphQLSchema({
+    query: queryType
+});
+
+export function start(done: any, appPort: number) {
+    const app = express();
+    const PORT = appPort || 3030;
+
+    app.get('graphql', graphqlHTTP({
+        schema: schema,
+        graphiql: true
+    }));
+
+    return app.listen(PORT, () => {
+        console.log(`listening on port ${PORT}`);
+        done();
+    });
+
+}
+
+export function stop(app: any, done: any) {
+    app.close();
+    done();
+}
+
+export function graphqlquery(app: any, query: any) {
+    let options = {
+        baseUrl : `http://localhost:${app.address().port}`,
+        uri : '/graphql',
+        qs : {graphqlQuery : query},
+        resolveWithFullResponse: true,
+        json: true
+    }
+    return request(options);
+}
